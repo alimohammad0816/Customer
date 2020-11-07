6b464de9ae82e22a550f912be98adeb842ffdaea
AliMohammad <alimohammad0816@gmail.com>
2020-11-07 16:50:10 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'alimohammad0816@gmail.com'
-EMAIL_HOST_PASSWORD = 'lwyvpsuhamofjrwa'
+EMAIL_HOST_USER = 'your email'
+EMAIL_HOST_PASSWORD = 'your password'
